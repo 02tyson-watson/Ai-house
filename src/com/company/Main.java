63028367d9a6e521a036a1015540a36dbe51c5c0
02tyson-watson <48022458+02tyson-watson@users.noreply.github.com>
2019-11-20 08:55:34 -0500

    Initialized firstName, lastName, and gender.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        String firstName = "place";
+        String lastName = "place";
+        String gender = "place";
     }
 }
